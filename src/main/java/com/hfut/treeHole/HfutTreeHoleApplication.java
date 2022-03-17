94bc4e03e1b8d411fc186540ddea4bad436866b6
ShenZhu <2587177743@qq.com>
2022-03-17 15:02:37 +0800

    2nd demo

--- src/main/java/com/hfut/treeHole/HfutTreeHoleApplication.java
+++ src/main/java/com/hfut/treeHole/HfutTreeHoleApplication.java
@@ -2,7 +2,7 @@ package com.hfut.treeHole;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//demo
+//2nd demo
 @SpringBootApplication
 public class HfutTreeHoleApplication {
 
